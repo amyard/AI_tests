22d08d225072224254d0d4acf9736f75738fcf72
Maksym Turenko <maksym@bundledocs.com>
2023-07-13 14:43:29 +0100

    small changes

--- llamaAI/DataConstants.cs
+++ llamaAI/DataConstants.cs
@@ -6,4 +6,5 @@ public static class DataConstants
 {
     public static string WizardLMPath = PathHelper.GetPath("Models", "wizardLM-7B.ggmlv3.q8_0.bin");
     public static string OpenLlamaPath = PathHelper.GetPath("Models", "open-llama-3b-q8_0.bin");
+    public static string GGMLPath = PathHelper.GetPath("Models", "ggml-model-f32-q8_0.bin");
 }
--- llamaAI/Program.cs
+++ llamaAI/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using llamaAI;
+using llamaAI.Helpers;
 
 string modelPath = DataConstants.WizardLMPath;
+
+// string modelPath = PathHelper.GetPath("Models", "change_model_name_here.bin");
 
 await TaskRunner.Run(modelPath);
