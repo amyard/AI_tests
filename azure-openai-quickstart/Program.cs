88f32643c58ad52be4de309d2eaed1f4ab8e289d
Maksym Turenko <maksym@bundledocs.com>
2024-08-12 14:42:00 +0100

    version 2 - terrible prompt

--- azure-openai-quickstart/Program.cs
+++ azure-openai-quickstart/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Text;
 using Azure;
 using Azure.AI.OpenAI;
+using Azure.AI.OpenAI.Chat;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
 using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using OpenAI.Chat;
 using SharpToken;
 using TiktokenSharp;
 
@@ -12,14 +14,16 @@ string key = "41f0c5f50f9348aa8a18f9faab9d3be2";
 string deploymentName = "gpt-35-turbo";
 string encodingName = "cl100k_base";
 
-OpenAIClient azureClient = new(new Uri(endpoint),  new AzureKeyCredential(key));
+AzureOpenAIClient azureClient = new(new Uri(endpoint),  new AzureKeyCredential(key));
+ChatClient chatClient = azureClient.GetChatClient(deploymentName);
+
 string pdfText = GetText(@"C:\LITROOT\Projects\dotNet\AI_tests\azure-openai-quickstart\Data\EngineersReport.pdf");
 int sharpTokenEncoding = SharpTokenEncoding(pdfText);
 int tiktokenSharpEncoding = TiktokenSharpEncoding(pdfText);
 Console.WriteLine($"sharpTokenEncoding - {sharpTokenEncoding}. tiktokenSharpEncoding - {tiktokenSharpEncoding}");
 
 // THIS WORKED AS EXPECTED !!!
-string prompt = @"Extract and provide the most relevant dates from the PDF document. Format the answer in 'Context' | 'Date'. Format the dates found in 'YYYY-MM-DD'. The output result should be in valid JSON format, with the dates sorted in ascending order. Additionally, provide a text summary of up to 10 sentences. The result should be stored in a valid JSON format like this:
+string prompt = @"Extract and provide only the most relevant dates from the PDF document.Ensure that the dates are unique in the output.Format the extracted dates in the 'YYYY-MM-DD' format.For each extracted date, include a brief context that describes the main information related to that date.The output should be sorted in ascending order by date.Summarize the text in up to 10 sentences.Store the results in a valid JSON format as follows:
 
 {
     'TextSummarization': '%TextSummary%',
@@ -30,21 +34,48 @@ string prompt = @"Extract and provide the most relevant dates from the PDF docum
     ]
 }";
 
+// Extension methods to use data sources with options are subject to SDK surface changes. Suppress the
+// warning to acknowledge and this and use the subject-to-change AddDataSource method.
+#pragma warning disable AOAI001
 
-var chatCompletionsOptions = new ChatCompletionsOptions() {
-    Messages = {
-        new ChatMessage(ChatRole.System, "You are a helpful AI Assistant"),
-        new ChatMessage(ChatRole.User, "The following information are from PDF document: " + pdfText),
-        new ChatMessage(ChatRole.User, prompt)
-    },
+var chatCompletionsOptions = new ChatCompletionOptions {
+    // MaxTokens = 1000,
+    Temperature = 0,
+    // Temperature = (float)0.7,
     MaxTokens = 1000,
-    Temperature = 0
+    // FrequencyPenalty = 0,
+    // PresencePenalty = 0,
+    // TopP = (float)0.95
+    // TopP = (float)0
 };
 
-Response<ChatCompletions> response = azureClient.GetChatCompletions(deploymentOrModelName: deploymentName, chatCompletionsOptions);
-var botResponse = response.Value.Choices.First().Message.Content;
+ChatCompletion completion = chatClient.CompleteChat(
+[
+    new SystemChatMessage("You are a helpful AI Assistant"),
+    new UserChatMessage("The following information are from PDF document: " + pdfText),
+    new UserChatMessage(prompt),
+], options: chatCompletionsOptions);
 
+var botResponse = completion.Content.First().ToString();
 Console.WriteLine(botResponse);
+
+var asd = "aaa";
+
+// AzureChatMessageContext onYourDataContext = completion.GetAzureMessageContext();
+//
+// if (onYourDataContext?.Intent is not null)
+// {
+//     Console.WriteLine($"Intent: {onYourDataContext.Intent}");
+// }
+// foreach (AzureChatCitation citation in onYourDataContext?.Citations ?? [])
+// {
+//     Console.WriteLine($"Citation: {citation.Content}");
+// }
+
+// Response<ChatCompletions> response = azureClient.GetChatCompletions(deploymentOrModelName: deploymentName, chatCompletionsOptions);
+// var botResponse = response.Value.Choices.First().Message.Content;
+//
+// Console.WriteLine(botResponse);
 
 
 
